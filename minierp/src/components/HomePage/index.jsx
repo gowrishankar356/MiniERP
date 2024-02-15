00daf9fd3643e3cafe53c4fbc613899679d84163
Badugu Gowri Shankar <gowrishankar356@gmail.com>
2024-02-15 01:23:14 -0500

    new changes

--- minierp/src/components/HomePage/index.jsx
+++ minierp/src/components/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./styles.module.css";
 import NavBar from "../NavBar";
+import SideNavBar from "../SideNavBar";
 
 const HomePage = () => {
   return (
     <div>
-        <NavBar></NavBar>
-   </div>
+      <NavBar></NavBar>
+      <SideNavBar></SideNavBar>
+    </div>
   );
 };
 
